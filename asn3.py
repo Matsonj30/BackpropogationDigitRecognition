140e41af952a4599325c8a383c1b0691ad70c8ba
Jared Matson <40784129+Matsonj30@users.noreply.github.com>
2021-12-01 14:00:34 -0700

    notes

--- asn3.py
+++ asn3.py
@@ -1,6 +1,59 @@
+from os import system
 import random
 import numpy
+import sys
+
+# Output is 0.12 and 0.2, but the actual output is 1 and 0. We will calculate the errors, and the w values will be adjusted off them (big error, big weight change and if small error, small weigh change)
+# Have to adjust weight values for hidden layers too, but can not calculate errors the same way in the hidden layers since they dont have a right value
+# **She provided initial weight values** BIAS NODES ARE at the input AND hidden layers 
+# 
+# After calculating the amount of error (1-0.12) = 0.88 SEEN As (z-y) z is target y is actual output
+# Apply fprime(incoming value to Y) -> (w46Y4 + w56Y5) * (z - y) = delta
+# w46 = w46 + Learning rate (given by me) *y4*delta6      y4 is calculated earlier, delta six is the delta above as it was for output 6
+# w56 + L.R * y5 * delta6
+# TO go further back and calculate delta 4, you get f'(w14Y1 + w24Y2 + w34Y3) x** w46 x delta6 ** This is the back propagation because we cant do x -y
+# do same for delta 5, but with its corresponding weights, but you will now consider w56 * delta 6
+# TO DO delta 1, f'(node*weight + node*weight of all inputs) x w14 x delta4, Carry the deltas backwards 
+# SINCE f1 is back prooagated by f4 and f5, you have to do both
+# NOW IT IS f'(node*weight + node*weight of all inputs) x w14 + delta 4 + (node*weight + node*weight of all inputs)) xw15 + delta 5
+# Now we are at the inputs, wx11 + wx11 + (learning rate * x1 * delta1)
+
+#Derivates, we want the derivative to be zero, Error is a parabolic fuction, and we want to find the minimum on that graph, which is where derivative is zero
+#if our derivative (or slope) is positive, we have to reduce weight, and increase it if the slope is negative
+#Error function does not necessarily have to be a parabola (like in the example), it could have many spots where the slope is zero, which could find a local minima (want global)
+# Finding the minimum here is dependent on the initial setting of weights** 
+
+# We need to check the gradient (derivative slope), we will move a small amount in the opposite direction of the gradient, and then recalculate the gradient in a new spot
+# calculate derivative of error/derivative of w
+# we have a negative now instead which means we have to take in the opposite direciton
+# on fucked slide, y is target output hw is my output -> either way we will end up not needing the derivative 
+#in the sigmoid function, g'(x) = g(x) * 1-g(x)
+#IF we use the sigmoid function as our logistic function, then we can calculate the weight using less values
 class Node:
-    def __init__(self, value, weight) -> None:
+    def __init__(self, value) -> None:
         self.value = value
-        self.weight = weight
+        self.weights = []
+
+def forwardPassingNetwork(terminalArguement):
+    inputLayer = []
+    awnser = []
+    if terminalArguement[0] == "train.txt":
+        file = open("D:/Programming/Repo/backPropogationDigitRecognition/BackpropogationDigitRecognition/"+terminalArguement[0],"r")
+    else:
+        file = open("D:/Programming/Repo/backPropogationDigitRecognition/BackpropogationDigitRecognition/"+terminalArguement[0], "r")
+    lines = file.readlines()
+    for line in lines:
+        for word in line.split():
+            if(len(word)) == 6: #making sure we dont include the last three numbers in each line
+                inputLayer.append(Node(word[0]))
+                for i in range(10):
+                    inputLayer[len(inputLayer)-1].weights.append(round(random.random(),2))
+                print(inputLayer[0].weights)
+            else: ##getting test result
+                awnser.append[word]
+                if len(awnser) == 3:
+                    print("WTF")
+                
+#print(round(random.random(),2))
+forwardPassingNetwork(["train.txt", "otherfile"])
+#forwardPassingNetwork(sys.argv)
