b9a6948018e9e948a5894e351c81c87f665cbedf
Jared Matson <40784129+Matsonj30@users.noreply.github.com>
2021-12-07 21:44:39 -0700

    final documentation

--- asn3.py
+++ asn3.py
@@ -13,26 +13,36 @@ from numpy.core.records import array
 #Given a test set, will train itself to recognize up to 3 numbers (1,7,8 in the current training set)
 #After training to an accuracy of 92%, the program will write the weights of each layer into two seperate files ("inputCorrectWeights.txt and hiddenCorrectWeights.txt") to be used for testing
 #If the person using this program runs the test file, the constructed neural network will grab the weights from the files listed above, and attempt to classify each test input, writing the results into test_output.txt
+#Each test and train input is 256 numbers, representing one tile in a 16x16 picture that makes up the handwritten image
 
 #class Node
 #This represents each specific node in the neural network
-#Value
-#Weights
-#Delta
-#Sigmoid
+#Value -> The value of the node
+#Weights -> A list of weights, a weight at index 0 for example will represent the weight value going to the 0th node
+#Delta -> The delta value of the node during backpropogation
 class Node:
     def __init__(self, value) -> None:
         self.value = value
         self.weights = []
         self.delta = 0 
 
-
+#sigmoid()
+#Given a value, will return its sigmoid activation function value
+#Parameters: Value -> The value that you want to see the activation value of
+#Returns -> sigmoid value
 def sigmoid(value):
     return 1/(1+ numpy.exp(neg(value)))
 
+#FPrime()
+#Given a sigmoid value, will return its Fprime value that is used in back propogation
+#Parameters: sigmoidValue -> Value that you want to Fprime
+#Returns -> FPrime value
 def FPrime(sigmoidValue):
     return(sigmoidValue*(1-sigmoidValue))
 
+#initializeNetwork()
+#Given terminal arguement, will construct the neural network basics, creating bias nodes and a unique node for each input
+#Parameters: terminalArguement -> The terminal arguements inputted
 def initializeNetwork(terminalArguement):
     inputLayer = []
     hiddenLayer = []
@@ -48,19 +58,27 @@ def initializeNetwork(terminalArguement):
     
     initializeWeights(inputLayer, hiddenLayer, outputLayer, terminalArguement)
 
-
+#initializeWeights()
+#Now that the neural network is initialized, we can assign initial weights to each node
+#If the terminalArguement inputs test.txt, this method will search inputCorrectWeights and hiddenCorrectWeights and assign weights from there
+#If the terminalArguement inputs train.txt, this method will assign random weights to each node in order to train it
+#Parameters:
+# inputLayer -> The input layer of the network
+# hiddenLayer -> The hidden layer of the network
+# outputLayer -> The outputLayer of the network
+# terminalArguement -> the terminal arguement inputted
 def initializeWeights(inputLayer, hiddenLayer, outputLayer, terminalArguement):
     if terminalArguement[1] == "test.txt": #we already tested, grab weights that had high accuracy
         inputWeightFile = open("D:/Programming/Repositories/BackpropogationDigitRecognition/BackpropogationDigitRecognition/inputCorrectWeights.txt","r")#desktop
         hiddenWeightFile = open("D:/Programming/Repositories/BackpropogationDigitRecognition/BackpropogationDigitRecognition/hiddenCorrectWeights.txt","r")#desktop
-
+        #read inputLayer weights
         lines = inputWeightFile.readlines() 
         index = 0
         for line in lines:
             for word in line.split():
                 inputLayer[index].weights.append(float(word))    
             index += 1
-
+        #read hiddenLayer weights
         index = 0
         lines = hiddenWeightFile.readlines()
         for line in lines:
@@ -77,25 +95,39 @@ def initializeWeights(inputLayer, hiddenLayer, outputLayer, terminalArguement):
             for i in range(3):
                 node.weights.append(round(random.uniform(0,0.05),5))  #before it was 0,0.05
         trainNetwork(inputLayer, hiddenLayer, outputLayer, terminalArguement, 0)
-    
+#writeToFile()
+#During the testing phase, this method will write the actual output of the neural network as opposed to its expected output
+#Parameters:
+#expectedOutputIndex -> The index of the correct output
+#ActualOutputIndex -> The index of the output of the training network for the iteration
 def writeToFile(expectedOutputIndex, ActualOutputIndex):
     file = open("D:/Programming/Repositories/BackpropogationDigitRecognition/BackpropogationDigitRecognition/train_output.txt","a")#desktop
     #file = open("D:/Programming/Repo/backPropogationDigitRecognition/BackpropogationDigitRecognition/train_output.txt", "a") #laptop
+    #writing the training algorithms output
     if(ActualOutputIndex == 0):
         file.write("1")
     elif(ActualOutputIndex == 1):
         file.write("8")
     elif(ActualOutputIndex == 2):
         file.write("9")
-
+    #writing the expected output
     if(expectedOutputIndex == 0):
         file.write("                           1\n")
     elif(expectedOutputIndex == 1):
         file.write("                           8\n")
     elif(expectedOutputIndex == 2):
         file.write("                           9\n")
-    file.close() #have to put the counter somehwere I guess?
+    file.close() 
 
+#trainNetwork()
+#This method controls the training process of the network, ensuring that it trains for another epoch if the current weights 
+#do not reach an accuracy of 92%+
+#Parameters:
+# inputLayer -> The input layer of the network
+# hiddenLayer -> The hidden layer of the network
+# outputLayer -> The outputLayer of the network
+# terminalArguement -> the terminal arguement inputted
+# epochNumber -> The current epoch the network is training on
 def trainNetwork(inputLayer, hiddenLayer, outputLayer, terminalArguement, epochNumber):
     epochNumber += 1
     fileToWrite = open("D:/Programming/Repositories/BackpropogationDigitRecognition/BackpropogationDigitRecognition/"+terminalArguement[2],"w")#desktop
@@ -115,7 +147,7 @@ def trainNetwork(inputLayer, hiddenLayer, outputLayer, terminalArguement, epochN
         answer = []
         indexInput = 1
         numberOfClassifications += 1 #per input
-        for word in line.split():
+        for word in line.split(): #for each square input in the 16x16 picture                                       
             if(len(word)) == 6: #making sure we dont include the last three numbers in each line
                 inputLayer[indexInput].value = int(float(word))
                 indexInput += 1
@@ -130,16 +162,16 @@ def trainNetwork(inputLayer, hiddenLayer, outputLayer, terminalArguement, epochN
                         expectedOutput = 2
                     actualOutput = passForward(inputLayer, hiddenLayer, outputLayer)
                     writeToFile(expectedOutput, actualOutput.index(max(actualOutput))) ##start here by writing to file
-                    if expectedOutput == actualOutput.index(max(actualOutput)):#the program correctly guessed the output
+                    if expectedOutput == actualOutput.index(max(actualOutput)):#the program correctly guessed the output, we want to find the index instead of the actual values to easier compare
                         correctlyClassified += 1
                         continue
                     else:
                         newValues = backPropogation(inputLayer, hiddenLayer, outputLayer, answer)
 
-    print(correctlyClassified / numberOfClassifications)
+    print(correctlyClassified / numberOfClassifications) #for each epoch, print accuracy of the network
     if(correctlyClassified / numberOfClassifications < 0.92):
         trainNetwork(newValues[0], newValues[1], newValues[2], terminalArguement, epochNumber)
-    else:
+    else: #if the network is above 92%, we can write its weight values down
         #file = open("D:/Programming/Repo/backPropogationDigitRecognition/BackpropogationDigitRecognition/train_output.txt", "a") #laptop
         file = open("D:/Programming/Repositories/BackpropogationDigitRecognition/BackpropogationDigitRecognition/"+terminalArguement[2],"a")#desktop
         file.write("\n")
@@ -147,8 +179,12 @@ def trainNetwork(inputLayer, hiddenLayer, outputLayer, terminalArguement, epochN
         file.close()
         print("epoch number"+ str(epochNumber))
         writeWeights(newValues[0], newValues[1])
-        #testNetwork(newValues[0], newValues[1], newValues[2], terminalArguement[1])
 
+#writeWeights()
+#After the testing process gets a accuracy of 92%, this method  will write the networks weights to a file to be used for testing
+#Parameters:
+# inputLayer -> The input layer of the network
+# hiddenLayer -> The hidden layer of the network
 def writeWeights(inputLayer, hiddenLayer):
     fileInput = open("D:/Programming/Repositories/BackpropogationDigitRecognition/BackpropogationDigitRecognition/inputCorrectWeights.txt","w")
     fileHidden = open("D:/Programming/Repositories/BackpropogationDigitRecognition/BackpropogationDigitRecognition/hiddenCorrectWeights.txt","w")
@@ -165,7 +201,13 @@ def writeWeights(inputLayer, hiddenLayer):
     fileHidden.close()
     
 
-
+#testNetwork()
+#This method will read the weights that were successful in the training phase, and test itself from the test file given
+#Parameters:
+# inputLayer -> The input layer of the network
+# hiddenLayer -> The hidden layer of the network
+# outputLayer -> The outputLayer of the network
+# terminalArguement -> the terminal arguement inputted
 def testNetwork(inputLayer, hiddenLayer, outputLayer, terminalArguement):
     file = open("D:/Programming/Repositories/BackpropogationDigitRecognition/BackpropogationDigitRecognition/"+terminalArguement[1],"r")#desktop
     #file = open("D:/Programming/Repo/backPropogationDigitRecognition/BackpropogationDigitRecognition/"+terminalArguement,"r") #laptop
@@ -174,19 +216,28 @@ def testNetwork(inputLayer, hiddenLayer, outputLayer, terminalArguement):
 
 
     lines = file.readlines()
-    for line in lines:
+    for line in lines:#read test file
         indexInput = 1
         for word in line.split():
             inputLayer[indexInput].value = int(float(word))
             indexInput += 1
         actualOutput = passForward(inputLayer, hiddenLayer, outputLayer)
         numberToWrite = actualOutput.index(max(actualOutput))
+        #write output to file
         if(numberToWrite == 0):
             fileToWrite.write("1\n")
         elif(numberToWrite == 1):
             fileToWrite.write("8\n")
         elif(numberToWrite == 2):
             fileToWrite.write("9\n")
+#backpropogation()
+#This method uses the backpropogation algorithm to determine the delta values of each node in order to later change weights(if the current test was wrong)
+#Parameters:
+# inputLayer -> The input layer of the network
+# hiddenLayer -> The hidden layer of the network
+# outputLayer -> The outputLayer of the network
+# expectedOutput -> the expected output of the test that was wrong
+# returns: The neural network with new weights
 def backPropogation(inputLayer,hiddenLayer,outputLayer, expectedOutput):
     delta = 0
     nodeIndex = 0 #iterate to determine which error values go to which output node
@@ -200,22 +251,27 @@ def backPropogation(inputLayer,hiddenLayer,outputLayer, expectedOutput):
     for hiddenNode in hiddenLayer: #sigmoid bias values
         nodeIndex = 0
         delta = 0
-        if biasNode == True:
+        if biasNode == True: #avoid bias nodes, we don't want to change their values
             for outputNode in outputLayer: 
-                delta += (FPrime(sigmoid(hiddenNode.value)) * outputNode.delta * hiddenNode.weights[nodeIndex]) ##probably wrong
+                delta += (FPrime(sigmoid(hiddenNode.value)) * outputNode.delta * hiddenNode.weights[nodeIndex]) 
                 nodeIndex += 1
             biasNode = False
             hiddenNode.delta = delta
         else:
             for outputNode in outputLayer:
-                delta += (FPrime(hiddenNode.value) * outputNode.delta * hiddenNode.weights[nodeIndex]) ##probably wrong
-                
+                delta += (FPrime(hiddenNode.value) * outputNode.delta * hiddenNode.weights[nodeIndex])
                 nodeIndex += 1
             hiddenNode.delta = delta
     return(changeWeights(inputLayer,hiddenLayer,outputLayer))
     #don't need delta in input layers, can just go on to changing weights now
 
-
+#changeWeights()
+#After the delta values are determined, this method will use them to actually change the weights of the input and hidden layers
+#Parameters:
+# inputLayer -> The input layer of the network
+# hiddenLayer -> The hidden layer of the network
+# outputLayer -> The outputLayer of the network
+# returns -> The whole network with changed weights
 def changeWeights(inputLayer,hiddenLayer,outputLayer):
     alpha = 1 
     inputLayerCopy = copy.deepcopy(inputLayer)
@@ -230,7 +286,7 @@ def changeWeights(inputLayer,hiddenLayer,outputLayer):
             else:
                 inputNode.weights[nodeIndex] = inputNode.weights[nodeIndex] + (alpha * hiddenNode.delta * inputNode.value)
                 nodeIndex += 1
-
+    #change weights in hidden layer
     for hiddenNode in hiddenLayerCopy:
         nodeIndex = 0
         for outputNode in outputLayerCopy:
@@ -239,6 +295,13 @@ def changeWeights(inputLayer,hiddenLayer,outputLayer):
 
     return(inputLayerCopy,hiddenLayerCopy, outputLayerCopy)
 
+#passForward()
+#This method will calculate the values of each node after given the values of the input nodes
+#Parameters:
+# inputLayer -> The input layer of the network
+# hiddenLayer -> The hidden layer of the network
+# outputLayer -> The outputLayer of the network
+# returns -> an array [x,y,z] which will present the probability of it being a 1,8, or 9. x = probability of 1, y = probability of 7 etc.
 def passForward(inputLayer, hiddenLayer, outputLayer): #reset values again somehow
     nodeNumber = 0
     for node in hiddenLayer:
